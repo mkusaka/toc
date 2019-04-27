e890d5f75b50759ebd4fa37a39675d479cca02bd
mkusaka <hinoshita1992@gmail.com>
2019-04-28 00:23:00 +0900

    feat: add cli settings

--- src/index.ts
+++ src/index.ts
@@ -15,61 +15,66 @@ interface parsedAst {
 
 const patternString = process.argv[2];
 
-function asyncGlob(globPattern: string): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    return glob(globPattern, function(err, files) {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(files);
-    });
-  })
-}
-
-asyncGlob(patternString)
-  .then((paths: string[]) => {
-    return paths.map(path => {
-      return {
-        path: path,
-        content: fs.readFileSync(path, { encoding: 'UTF-8' })
-      };
+if (!patternString || patternString.length == 0) {
+  console.log('glob pattern string to argument required.');
+  process.exit(1);
+} else {
+  function asyncGlob(globPattern: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      return glob(globPattern, function(err, files) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(files);
+      });
     })
-  })
-  .then((contents: {
-    path: string,
-    content: string
-  }[]) => {
-    return contents.map(
-      ({ path, content }): { path: string; ast: parsedAst } => {
+  }
+
+  asyncGlob(patternString)
+    .then((paths: string[]) => {
+      return paths.map(path => {
         return {
           path: path,
-          ast: mdParser.parse(content)
+          content: fs.readFileSync(path, { encoding: 'UTF-8' })
         };
-      }
-    );
-  })
-  .then(asts => {
-    return asts.map(({ path, ast }) => {
-      return {
-        filePath: path,
-        headers: ast.children.filter(e => e.type === mdParser.Syntax.Header).map(e => {
+      })
+    })
+    .then((contents: {
+      path: string,
+      content: string
+    }[]) => {
+      return contents.map(
+        ({ path, content }): { path: string; ast: parsedAst } => {
           return {
-            value: e.children[0].value,
-            depth: e.depth
-          }
-        })
-      }
-    }).filter(e => e.headers.length > 0);
-  })
-  .then(contentFiles => {
-    return contentFiles.map(({ filePath, headers }) => {
-      const headersText = headers.map(({ value, depth }) => {
-        const indent = ' '.repeat(depth);
-        return `${indent}- [${value}](${filePath}#${encodeURI(value)})`
-      }).join("\n")
-      return `[${filePath.replace(path.extname(filePath), "")}](${filePath})
+            path: path,
+            ast: mdParser.parse(content)
+          };
+        }
+      );
+    })
+    .then(asts => {
+      return asts.map(({ path, ast }) => {
+        return {
+          filePath: path,
+          headers: ast.children.filter(e => e.type === mdParser.Syntax.Header).map(e => {
+            return {
+              value: e.children[0].value,
+              depth: e.depth
+            }
+          })
+        }
+      }).filter(e => e.headers.length > 0);
+    })
+    .then(contentFiles => {
+      return contentFiles.map(({ filePath, headers }) => {
+        const headersText = headers.map(({ value, depth }) => {
+          const indent = ' '.repeat(depth);
+          return `${indent}- [${value}](${filePath}#${encodeURI(value)})`
+        }).join("\n")
+        return `[${filePath.replace(path.extname(filePath), "")}](${filePath})
 ${headersText}
 `
-    }).join("\n")
-  })
-  .then(e => console.log(e));
+      }).join("\n")
+    })
+    .then(e => console.log(e));
+}
