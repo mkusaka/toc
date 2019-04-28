dca3b2c5c9cbabbf40adbfa16180faae5241179b
mkusaka <hinoshita1992@gmail.com>
2019-04-28 14:55:31 +0900

    feat: export functions
    now, we can use asyncGlob, globAsts, globAstlike, md, standardStream funcion

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as mdParser from "@textlint/markdown-to-ast";
 import * as fs from 'fs';
 import * as path from 'path';
 
-interface parsedAst {
+export interface parsedAst {
   children: {
     type: string,
     children: {
@@ -15,13 +15,13 @@ interface parsedAst {
 
 const patternString = process.argv[2];
 
-if (!patternString || patternString.length == 0) {
-  console.log('glob pattern string to argument required.');
-  process.exit(1);
-}
-
-export function asyncGlob(globPattern: string): Promise<string[]> {
+export function asyncGlob(globPattern: string = patternString): Promise<string[]> {
   return new Promise((resolve, reject) => {
+    if (!globPattern || globPattern.length == 0) {
+      console.log('glob pattern string to argument required.');
+      return reject();
+    }
+
     return glob(globPattern, function(err, files) {
       if (err) {
         return reject(err);
@@ -31,42 +31,50 @@ export function asyncGlob(globPattern: string): Promise<string[]> {
   })
 }
 
-asyncGlob(patternString)
-  .then((paths: string[]) => {
-    return paths.map(path => {
-      return {
-        path: path,
-        content: fs.readFileSync(path, { encoding: 'UTF-8' })
-      };
-    })
-  })
-  .then((contents: {
-    path: string,
-    content: string
-  }[]) => {
-    return contents.map(
-      ({ path, content }): { path: string; ast: parsedAst } => {
+export const globAsts = (pattern = patternString) => {
+  return asyncGlob(pattern)
+    .then((paths: string[]) => {
+      return paths.map(path => {
         return {
           path: path,
-          ast: mdParser.parse(content)
+          content: fs.readFileSync(path, { encoding: 'UTF-8' })
         };
-      }
-    );
-  })
-  .then(asts => {
-    return asts.map(({ path, ast }) => {
-      return {
-        filePath: path,
-        headers: ast.children.filter(e => e.type === mdParser.Syntax.Header).map(e => {
+      });
+    })
+    .then((contents: { path: string; content: string }[]) => {
+      return contents.map(
+        ({ path, content }): { path: string; ast: parsedAst } => {
           return {
-            value: e.children[0].value,
-            depth: e.depth
-          }
-        })
-      }
-    }).filter(e => e.headers.length > 0);
-  })
-  .then(contentFiles => {
+            path: path,
+            ast: mdParser.parse(content)
+          };
+        }
+      );
+    });
+}
+
+export const globAstlike = (pattern = patternString) => {
+  return globAsts(pattern).then(asts => {
+    return asts
+      .map(({ path, ast }) => {
+        return {
+          filePath: path,
+          headers: ast.children
+            .filter(e => e.type === mdParser.Syntax.Header)
+            .map(e => {
+              return {
+                value: e.children[0].value,
+                depth: e.depth
+              };
+            })
+        };
+      })
+      .filter(e => e.headers.length > 0);
+  });
+}
+
+export const md = (pattern = patternString) => {
+  return globAstlike(pattern).then(contentFiles => {
     return contentFiles.map(({ filePath, headers }) => {
       const headersText = headers.map(({ value, depth }) => {
         const indent = ' '.repeat(depth);
@@ -76,5 +84,9 @@ asyncGlob(patternString)
 ${headersText}
 `
     }).join("\n")
-  })
-  .then(e => console.log(e));
+  });
+}
+
+export const standardStream = (pattern = patternString) => {
+  return md(pattern).then(e => console.log(e))
+}
