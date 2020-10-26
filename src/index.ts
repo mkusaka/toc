dd32523d84fbe79418064d48142509e9cf7cd8a2
mkusaka <hinoshita1992@gmail.com>
2020-10-26 16:36:47 +0900

    fix: format and update changed api

--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,60 @@ import * as path from "path";
 
 export interface parsedAst {
   children: {
-    type: string,
+    type: string;
     children: {
-      value: string,
-    }[],
-    depth: number
-  }[],
+      value: string;
+    }[];
+    depth: number;
+  }[];
 }
 
 const patternString = process.argv[2];
 const aggregateFilePath = process.argv[3];
 const showDepth = parseInt(process.argv[4]);
 
-export const globAsts = (pattern = patternString, aggregateFile: string = aggregateFilePath) => {
-  return fg.async(pattern)
-    .then(paths => {
+export const globAsts = (
+  pattern = patternString,
+  aggregateFile: string = aggregateFilePath
+) => {
+  return fg(pattern)
+    .then((paths) => {
       return {
         aggregateFile: aggregateFilePath,
-        paths
+        paths,
       };
     })
     .then(({ aggregateFile, paths }) => {
       return {
         aggregateFile,
-        contents: paths.map(path => {
+        contents: paths.map((path) => {
           return {
             path: path as string,
-            content: fs.readFileSync(path as string, { encoding: 'UTF-8' })
+            content: fs.readFileSync(path as string, { encoding: "utf-8" }),
           };
-        })
+        }),
       };
     })
     .then(({ aggregateFile, contents }) => {
       return {
         aggregateFile,
-        asts: contents.map(
-          ({ path, content }): { path: string; ast: parsedAst } => {
-            return {
-              path: path,
-              ast: mdParser.parse(content)
-            };
-          }
-        )
+        asts: contents.map(({ path, content }): {
+          path: string;
+          ast: parsedAst;
+        } => {
+          return {
+            path: path,
+            ast: mdParser.parse(content),
+          };
+        }),
       };
     });
-}
+};
 
-export const globAstlike = (pattern = patternString, aggregateFile: string = aggregateFilePath) => {
+export const globAstlike = (
+  pattern = patternString,
+  aggregateFile: string = aggregateFilePath
+) => {
   return globAsts(pattern, aggregateFile).then(({ aggregateFile, asts }) => {
     return {
       aggregateFile,
@@ -60,57 +67,93 @@ export const globAstlike = (pattern = patternString, aggregateFile: string = agg
           return {
             filePath: path,
             headers: ast.children
-              .filter(e => e.type === mdParser.Syntax.Header)
-              .map(e => {
+              .filter((e) => e.type === mdParser.Syntax.Header)
+              .map((e) => {
                 return {
                   value: e.children[0].value,
-                  depth: e.depth
+                  depth: e.depth,
                 };
               })
-              .filter(({ depth }) => showDepth ? depth <= showDepth : true)
+              .filter(({ depth }) => (showDepth ? depth <= showDepth : true)),
           };
         })
-        .filter(e => e.headers.length > 0)
+        .filter((e) => e.headers.length > 0),
     };
   });
-}
+};
 
 const relativeStrings = [".", ".."];
-const removeRelativePath = (pathArray: string[]) => pathArray.filter(path => !relativeStrings.includes(path));
+const removeRelativePath = (pathArray: string[]) =>
+  pathArray.filter((path) => !relativeStrings.includes(path));
 
-export const md = (pattern = patternString, aggregateFile: string = aggregateFilePath) => {
-  return globAstlike(pattern, aggregateFile).then(({ aggregateFile, contentFiles }) => {
-    return contentFiles.map(({ filePath, headers }) => {
-      let fixedFilePath = filePath;
-      // TODO: isArrregateFile? like varable as `aggregateFile && aggregateFile.length > 0` to refactor some shared variable.
-      const aggregate = aggregateFile && aggregateFile.length > 0 ? aggregateFile.split("/") : [];
-      const removeRelativeStringAggregate = removeRelativePath(aggregate)
-      if (removeRelativeStringAggregate.length > 0) {
-        const aggregated = filePath.split("/");
-        const removeRelativeStringAggregated = removeRelativePath(aggregated)
-        const min = Math.min(...[removeRelativeStringAggregate, removeRelativeStringAggregated].map(e => e.length));
-        for (let i = 0; i < min; i++) {
-          if (removeRelativeStringAggregate[0] === removeRelativeStringAggregate[0]) {
-            removeRelativeStringAggregated.shift();
-            removeRelativeStringAggregate.shift();
-          } else {
-            break;
+export const md = (
+  pattern = patternString,
+  aggregateFile: string = aggregateFilePath
+) => {
+  return globAstlike(pattern, aggregateFile).then(
+    ({ aggregateFile, contentFiles }) => {
+      return contentFiles
+        .map(({ filePath, headers }) => {
+          let fixedFilePath = filePath;
+          // TODO: isArrregateFile? like varable as `aggregateFile && aggregateFile.length > 0` to refactor some shared variable.
+          const aggregate =
+            aggregateFile && aggregateFile.length > 0
+              ? aggregateFile.split("/")
+              : [];
+          const removeRelativeStringAggregate = removeRelativePath(aggregate);
+          if (removeRelativeStringAggregate.length > 0) {
+            const aggregated = filePath.split("/");
+            const removeRelativeStringAggregated = removeRelativePath(
+              aggregated
+            );
+            const min = Math.min(
+              ...[
+                removeRelativeStringAggregate,
+                removeRelativeStringAggregated,
+              ].map((e) => e.length)
+            );
+            for (let i = 0; i < min; i++) {
+              if (
+                removeRelativeStringAggregate[0] ===
+                removeRelativeStringAggregate[0]
+              ) {
+                removeRelativeStringAggregated.shift();
+                removeRelativeStringAggregate.shift();
+              } else {
+                break;
+              }
+            }
+            fixedFilePath = removeRelativeStringAggregated.join("/");
           }
-        }
-        fixedFilePath = removeRelativeStringAggregated.join("/");
-      }
-      const headersText = headers.map(({ value, depth }) => {
-        const indent = "  ".repeat(depth - 1);
-        return `${indent}- [${value}](${fixedFilePath && fixedFilePath.length > 0 ? `./${fixedFilePath}` : ""}#${encodeURI(value.replace(/@|\//g, "").replace(/ /g, "-"))})`;
-      }).join("\n")
-      const fileMdPathText = fixedFilePath && fixedFilePath.length > 0 ? `[${fixedFilePath.replace(path.extname(fixedFilePath), "")}](./${fixedFilePath})` : "";
-      return `${fileMdPathText}
+          const headersText = headers
+            .map(({ value, depth }) => {
+              const indent = "  ".repeat(depth - 1);
+              return `${indent}- [${value}](${
+                fixedFilePath && fixedFilePath.length > 0
+                  ? `./${fixedFilePath}`
+                  : ""
+              }#${encodeURI(value.replace(/@|\//g, "").replace(/ /g, "-"))})`;
+            })
+            .join("\n");
+          const fileMdPathText =
+            fixedFilePath && fixedFilePath.length > 0
+              ? `[${fixedFilePath.replace(
+                  path.extname(fixedFilePath),
+                  ""
+                )}](./${fixedFilePath})`
+              : "";
+          return `${fileMdPathText}
 ${headersText}
-`
-    }).join("\n")
-  });
-}
+`;
+        })
+        .join("\n");
+    }
+  );
+};
 
-export const standardStream = (pattern = patternString, aggregateFile: string = aggregateFilePath) => {
-  return md(pattern, aggregateFile).then(e => console.log(e));
-}
+export const standardStream = (
+  pattern = patternString,
+  aggregateFile: string = aggregateFilePath
+) => {
+  return md(pattern, aggregateFile).then((e) => console.log(e));
+};
